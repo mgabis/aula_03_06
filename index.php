9d59aa42b03c469455eed80d2c5dad574b27225c
mgabis <gabis.97.mgs@gmail.com>
2015-06-03 21:48:30 -0300

    index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     </head>
     <body>
-        <div class="container" >
+        <div class="container">
 
             <div class="row">
                 <div class="col-md-6">
@@ -18,20 +18,20 @@
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Nome</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" placeholder="Nome">
+                                <input type="text" id="input-nome" class="form-control" placeholder="Nome">
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Endereco</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" placeholder="Endereco">
+                                <input type="text" id="input-endereco" class="form-control" placeholder="Endereço">
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Sexo</label>
                             <div class="col-sm-10">
                                 <label class="radio-inline">
-                                    <input type="radio" value="F" name="sexo"> Feminino 
+                                    <input type="radio" id="input-sexo" value="F" name="sexo"> Feminino 
                                 </label>
                                 <label class="radio-inline">
                                     <input type="radio" value="M" name="sexo"> Masculino 
@@ -52,13 +52,28 @@
                         </div>
                         <div class="form-group">
                             <div class="col-sm-offset-2 col-sm-10">
-                                <button type="submit" class="btn btn-success">Cadastrar</button>
+                                <button type="submit" id="btn-enviar" class="btn btn-success">Cadastrar</button>
                             </div>
                         </div>
-                    </form></div>
+                    </form>
+                </div>
                 <div class="col-md-6"></div>
             </div>
+        </div>
 
-        </div>    
-    </body>
+        <script>
+            $('#btn-enviar').click(function(){
+              if($('#input-nome').val()==''){
+                  alert("Preencha o nome");
+              }
+            })
+            
+             $('#btn-enviar').click(function(){
+              if($('#input-endereco').val()==''){
+                  alert("Preencha o endereço");
+              }
+            })
+                       
+        </script> 
+    </body> 
 </html>
